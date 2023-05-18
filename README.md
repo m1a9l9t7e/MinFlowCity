a978e347a6c23595bf27ae6a9776523755d5f064
Malte <m1a9l9t7e@gmail.com>
2023-05-18 17:10:09 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# MinFlowCity
+# MinFlowCity: City Graph Analysis and Visualization
+
+![image](media/berlin.png)
+
+## Overview
+
+MinFlowCity is a Python program leveraging the power of several robust libraries such as `osmnx`, `networkx`,
+and `manim` to perform city graph analysis and visualization. The goal is to construct a city's network graph with
+different node types (such as suppliers, consumers, and neutrals) and then find the optimal solution for minimum cost
+flow. The final output of the project is a visualization of this graph, which allows a more intuitive understanding of
+the city's network structure and its optimal flow solution.
+
+## Breakdown of Program
+
+### City Graph Generation
+
+The city graph generation utilizes the `osmnx` library which accesses OpenStreetMap (OSM) data for a specific location.
+The raw data is then transformed into a Graph Data Frame used to construct our city graph. During the construction of
+the city graph, nodes are assigned specific types: supplier, consumer, or neutral. The number of each node type is
+adjustable.
+
+### Minimum Cost Flow Solution
+
+Once the city graph has been generated, the program calculates the minimum cost flow solution for the network using
+the `networkx` library. 
+
+* Each supply node provides "unlimited" flow (using super supply node idea)
+* Each consumer node must consumer exactly one flow
+* Each edge has unlimited capacity and a cost equivalent to its euclidian distance.
+
+The program uses the `min_cost_flow` method from `networkx` to find the most cost-effective way to
+distribute flow from suppliers to consumers.
+
+### Visualization
+
+The final step in the program is to visualize the graph and the optimal flow solution. The `manim` library is used for
+this purpose. Each node type is depicted by a different color and size for easy identification. Additionally, the edges
+part of the optimal solution are highlighted, providing a clear visual representation of the flow distribution across
+the network.
+
+## Adaptation
+
+You can change the city or area for the analysis by adjusting the coordinates and distance parameters in
+the `generate_city_graph` function. Similarly, you can also change the distribution of node types by altering the number
+of suppliers and consumers.
---
+++ main.py
@@ -0,0 +1,161 @@
+import osmnx as ox
+import networkx as nx
+import random
+from manim import *
+import numpy as np
+import os
+
+DOWNLOAD_PATH = './osm_cache'
+
+
+class MinCostEdges(MovingCameraScene):
+    def construct(self):
+        # Get graph
+        nodes, edges = generate_city_graph()
+
+        # Calculate Solution
+        start = time.time()
+        state = self.solve(nodes, edges, False)
+        end = time.time()
+        print(f'Solution found in {end - start:.2f}s')
+
+        # Draw Solution
+        print('Drawing Solution...')
+        self.adjust_camera_settings(nodes)
+        self.draw_graph(nodes, edges, state, scale=0.5)
+
+    def solve(self, nodes, edges, equal_weights=True):
+        # Create directed graph
+        graph = nx.DiGraph()
+
+        # Add nodes
+        num_consumers = 0
+        for index, node in nodes.items():
+            if node['type'] == 'consumer':
+                graph.add_node(index, demand=1)
+                num_consumers += 1
+            else:
+                graph.add_node(index, demand=0)
+
+        graph.add_node('super-supplier', demand=-num_consumers)
+
+        # Add edges
+        node_to_position = {index: np.array(node['pos']) for index, node in nodes.items()}
+        for u, v in edges:
+            distance = 1 if equal_weights else int(np.linalg.norm(node_to_position[u] - node_to_position[v]))
+            graph.add_edge(u, v, weight=distance, capacity=num_consumers)
+            graph.add_edge(v, u, weight=distance, capacity=num_consumers)
+
+        # Connect super-supplier to the rest of the graph
+        for index, node in nodes.items():
+            if node['type'] == 'supplier':
+                graph.add_edge('super-supplier', index, weight=0, capacity=num_consumers)
+
+        # Compute minimum cost flow
+        return nx.min_cost_flow(graph)
+
+    def draw_graph(self, nodes, edges, mapping=None, scale=1.0):
+        node_color_map = {
+            "supplier": BLUE_C,
+            "consumer": GREEN_C,
+            "neutral": GRAY,
+        }
+
+        node_radius_map = {
+            "supplier": 0.6 * scale,
+            "consumer": 0.3 * scale,
+            "neutral": 0.1 * scale,
+        }
+
+        # Draw edges
+        for u, v in edges:
+            start_node, end_node = nodes[u], nodes[v]
+            sx, sy = start_node["pos"]
+            ex, ey = end_node["pos"]
+            # if edge is part of solution
+            if mapping[u][v] > 0 or mapping[v][u] > 0:
+                line = Line((sx, sy, 0), (ex, ey, 0), color=GREEN_C, stroke_width=8 * scale)
+            else:
+                line = Line((sx, sy, 0), (ex, ey, 0), color=GRAY, stroke_width=3 * scale)
+            self.add(line)
+
+        # Draw nodes
+        for node in nodes.values():
+            node_type = node["type"]
+            circle = Circle(radius=node_radius_map[node_type], color=node_color_map[node_type], fill_opacity=1)
+            x, y = node["pos"]
+            circle.shift(np.array([x, y, 0]))
+            self.add(circle)
+
+    def adjust_camera_settings(self, nodes):
+        positions = [node['pos'] for index, node in nodes.items()]
+        # calculate dimensions
+        min_x, min_y = np.amin(np.array(list(positions)), axis=0)
+        max_x, max_y = np.amax(np.array(list(positions)), axis=0)
+        width = max_x - min_x
+        height = max_y - min_y
+        # move camera
+        camera_size = [width, height]
+        camera_position = np.array(camera_size) / 2 + np.array([min_x, min_y])
+        print("size: {}, camera_pos: {}".format(camera_size, camera_position))
+        self.move_camera(camera_size, camera_position, duration=1, border_scale=1.1, shift=[0, 0])
+
+    def move_camera(self, camera_size, camera_position, duration=1, border_scale=1.1, shift=[0.0, 0.0], resolution=[16, 9]):
+        camera_position = [camera_position[0] + shift[0], camera_position[1] + shift[1]]
+        self.play(
+            self.camera.frame.animate.move_to((camera_position[0], camera_position[1], 0)),
+            run_time=duration / 2
+        )
+        if camera_size[0] / resolution[0] > camera_size[1] / resolution[1]:
+            self.play(
+                self.camera.frame.animate.set_width(camera_size[0] * border_scale),
+                run_time=duration / 2
+            )
+        else:
+            self.play(
+                self.camera.frame.animate.set_height(camera_size[1] * border_scale),
+                run_time=duration / 2
+            )
+
+
+def generate_city_graph(point=(52.5200, 13.4050), distance=4000, num_supplier=15, num_consumer=400):
+    # Load raw osm graph
+    graph = load_osm_graph(point, distance)
+    nodes, edges = ox.graph_to_gdfs(graph)
+    print(f'Graph loaded: {len(nodes)} nodes, {len(edges)} edges')
+
+    # assign types to nodes
+    node_types = ["supplier"] * num_supplier + ["consumer"] * num_consumer + ["neutral"] * (len(nodes) - num_supplier - num_consumer)
+    random.shuffle(node_types)
+    nodes_formatted = dict()
+    for i, (index, node) in enumerate(nodes.iterrows()):
+        nodes_formatted[index] = {
+            "pos": (node['x'] * 500, node['y'] * 500),
+            "type": node_types[i]
+        }
+
+    # only keep unique edges
+    unique_edges = set()
+    for index, edge in edges.iterrows():
+        unique_edges.add(index[:2])
+
+    return nodes_formatted, list(unique_edges)
+
+
+def load_osm_graph(point, distance):
+    os.makedirs(DOWNLOAD_PATH, exist_ok=True)
+    # Check if the data is already cached locally
+    path = os.path.join(DOWNLOAD_PATH, f'{point}_{distance}.graphml')
+    if os.path.exists(path):
+        # Load the graph from a cached file
+        graph = ox.load_graphml(path)
+    else:
+        # Retrieve the OSM street network data for the place
+        graph = ox.graph_from_point(point, dist=distance, network_type="drive")
+        ox.save_graphml(graph, path)
+    return graph
+
+
+if __name__ == '__main__':
+    scene = MinCostEdges()
+    scene.render()
